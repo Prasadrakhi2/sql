565c1e4cc6cd815ae6c90b52eb58afa6d27b8eee
rakhi prasad <142569721+Prasadrakhi2@users.noreply.github.com>
2024-07-09 16:20:37 +0000

    changes

---
+++ basics/09_Aggregates.sql
@@ -0,0 +1,15 @@
+"use or perfor the function on two or more than two values"
+
+type->
+1.COUNT()  -> "return the no of rows in database table"
+2.SUM()    -> "return the total sum of the no of coloumn"
+3.AVG()    -> "calcute the average of the set of values"
+4.MIN()    -> "return the min value of the pertculer coloumn"
+5.MAX()    -> "return the max value of the pertculer coloumn"
+
+ex->
+1. SELECT COUNT(age) FROM example
+2. SELECT SUM(age) FROM example
+3. SELECT MIN(age) FROM example
+4. SELECT MAX(age) FROM example
+5. SELECT AVG(age) FROM example
