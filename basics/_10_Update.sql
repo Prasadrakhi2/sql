83f68d886cb07a750ac8d151a43bcbab29470d02
rakhi prasad <142569721+Prasadrakhi2@users.noreply.github.com>
2024-07-11 16:01:30 +0000

    changes

---
+++ basics/_10_Update.sql
@@ -0,0 +1,8 @@
+Update -> "update the perticuler coloumn's row value"
+
+Syntex -> UPDATE table_name
+          SET field1 = new-value1,
+          field2 = new-value2[WHERE Clouse]
+
+"if show the error => you are using the safe mode then active the primary key(pk)"
+ex => 
---
+++ basics/_11Deleat.sql
@@ -0,0 +1,8 @@
+-- "deleate the particular row but the id to the next row will same not assigned the deleted id to the next row"
+
+Syntex -> DELEATE FROM table_name [WHERE clouse]
+
+ex-> 1.DELETE FROM example WHERE sn = 4
+     2.DELETE FROM example WHERE name = "shubham"
+     3.DELETE FROM example WHERE sn IN (6,8)
+     "if we insert the value now than the id start from 9"
